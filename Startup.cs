32e2b1509dcb91bdaa2ce264800f961842119cb0
dmazumder <dmazumder@hhaexchange.com>
2023-07-10 11:15:39 +0530

    feat: modified greet again

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ namespace HelloWeb
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello World Dipanjan, How are you !!!!");
+                return context.Response.WriteAsync("Hello World Dipanjan, How are you , all good!!!!");
             });
         }
     }
